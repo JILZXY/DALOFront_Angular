96c96d48151bd30096ef39373c47211617637d2f
Ali <lriozcristobal@gmail.com>
2025-11-04 10:33:12 -0600

    Jeshua, Guzman, Ali, Alberto

--- src/app/abog_pages/foro/foro.spec.ts
+++ src/app/abog_pages/foro/foro.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Foro } from './foro';
+import { Foro  } from './foro';
 
 describe('Foro', () => {
-  let component: Foro;
-  let fixture: ComponentFixture<Foro>;
+  let component: Foro ;
+  let fixture: ComponentFixture<Foro >;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Foro]
+      imports: [Foro ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Foro);
+    fixture = TestBed.createComponent(Foro );
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
