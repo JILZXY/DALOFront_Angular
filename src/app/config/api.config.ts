7793091eada1f840fc0bfcfd378d9c2443a43963
Luissmont <luispm2424@gmail.com>
2025-11-21 20:47:32 -0600

    Configuracion Base // URL LOCAL

---
+++ src/app/config/api.config.ts
@@ -0,0 +1,33 @@
+export const API_CONFIG = {
+    baseUrl: 'http://localhost:7000',
+    endpoints: {
+
+        login: '/api/auth/login',
+        register: '/api/auth/register',
+        me: '/api/usuarios/me',
+
+        estados: '/api/estados',
+        municipios: '/api/municipios',
+
+        consultas: '/api/consultas',
+        misConsultas: '/api/consultas/mis-consultas',
+        respuestas: '/api/respuestas',
+
+        abogados: '/api/abogados',
+        abogadosFiltro: '/api/abogados/filtro',
+
+        bufetes: '/api/bufetes',
+        misBufetes: '/api/bufetes/mis-bufetes',
+        solicitudesBufete: '/api/solicitudes-bufete',
+
+        chats: '/api/chats',
+        mensajes: '/api/chats',
+
+        calificaciones: '/api/calificaciones',
+        calificacionesBufete: '/api/calificaciones-bufete',
+
+        reportes: '/api/reportes',
+        motivosReporte: '/api/motivos-reporte',
+    }
+};
+
