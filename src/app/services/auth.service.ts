446223036dcdce5f8f975729aacfa560c095d73d
Luis-Ali44 <lriozcristobal@gmail.com>
2025-11-22 18:16:32 -0600

    auth.service iniciarsesion/registrarnuevousuario/obtenerusuarioactualautenticado/obtenerusuarioporemail/obtenerusuariosinactivos/activarusuario etc

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+import { LoginRequest, RegisterRequest, AuthResponse, Usuario } from '../models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    private baseUrl = API_CONFIG.baseUrl;
+
+    constructor(private http: HttpClient) { }
+
+
+    login(credentials: LoginRequest): Observable<AuthResponse> {
+        return this.http.post<AuthResponse>(
+            `${this.baseUrl}${API_CONFIG.endpoints.login}`,
+            credentials
+        );
+    }
+
+
+    register(data: RegisterRequest): Observable<AuthResponse> {
+        return this.http.post<AuthResponse>(
+            `${this.baseUrl}${API_CONFIG.endpoints.register}`,
+            data
+        );
+    }
+
+
+    getCurrentUser(): Observable<Usuario> {
+        return this.http.get<Usuario>(
+            `${this.baseUrl}${API_CONFIG.endpoints.me}`
+        );
+    }
+
+
+    getUserByEmail(email: string): Observable<Usuario> {
+        return this.http.get<Usuario>(
+            `${this.baseUrl}/api/auth/email/${email}`
+        );
+    }
+
+
+    getUsuariosInactivos(): Observable<Usuario[]> {
+        return this.http.get<Usuario[]>(
+            `${this.baseUrl}/api/auth/inactivos`
+        );
+    }
+
+
+    activarUsuario(id: string): Observable<void> {
+        return this.http.patch<void>(
+            `${this.baseUrl}/api/auth/${id}/activar`,
+            {}
+        );
+    }
+
+
+    getAllUsuarios(): Observable<Usuario[]> {
+        return this.http.get<Usuario[]>(
+            `${this.baseUrl}/api/usuarios`
+        );
+    }
+
+
+    getUsuarioById(id: string): Observable<Usuario> {
+        return this.http.get<Usuario>(
+            `${this.baseUrl}/api/usuarios/${id}`
+        );
+    }
+
+
+    updateUsuario(id: string, data: Partial<Usuario>): Observable<Usuario> {
+        return this.http.put<Usuario>(
+            `${this.baseUrl}/api/usuarios/${id}`,
+            data
+        );
+    }
+
+
+    deleteUsuario(id: string): Observable<void> {
+        return this.http.delete<void>(
+            `${this.baseUrl}/api/usuarios/${id}`
+        );
+    }
+}
